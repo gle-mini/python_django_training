6848c036e6050bb092a509e417bf0c0175fe0169
Gurvan Le Minier <gleminier2@gmail.com>
2024-05-01 01:59:40 +0200

    Ex05 [OK] all_in.py [OK] ---delete comments test_all_in.py [OK]

--- 0_Starting/ex05/all_in.py
+++ 0_Starting/ex05/all_in.py
@@ -18,13 +18,10 @@ def analyze_inputs(input_string):
         "CO": "Denver"
     }
 
-    # Create reverse lookup for capital cities to states
     capital_to_state_lower = {v.lower(): k for k, v in capital_cities.items()}
-    state_to_capital_lower = {k.lower(): v for k, v in states.items()}
-    
+    state_to_capital_lower = {k.lower(): v for k, v in states.items()} 
     state_to_capital_upper = {v: k for k, v in states.items()}
 
-    # Normalize input by trimming extra spaces and setting to lower case
     expressions = [expr.strip() for expr in input_string.split(',')]
 
     results = []
@@ -32,22 +29,19 @@ def analyze_inputs(input_string):
         if (expr == ""):
             continue
         if expr.lower() in state_to_capital_lower:
-            # It's a recognized capital city
             results.append(f"{capital_cities.get(state_to_capital_lower.get(expr.lower()))} is the capital of {state_to_capital_upper.get(state_to_capital_lower.get(expr.lower()))}")
         elif expr.lower() in capital_to_state_lower:
-            # It's a recognized state
             results.append(f"{capital_cities.get(capital_to_state_lower.get(expr.lower()))} is the capital of {state_to_capital_upper.get(capital_to_state_lower.get(expr.lower()))}")
         else:
-            # Neither a recognized state nor capital
             results.append(f"{expr} is neither a capital city nor a state")
     
     return results
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:  # Check for exactly one argument
+    if len(sys.argv) == 2:
         input_string = sys.argv[1]
         results = analyze_inputs(input_string)
-        if results:  # If results list is not empty
+        if results:
             for result in results:
                 print(result)
  
